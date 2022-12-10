a4a53fc4a3a200d0a1e1cb96d0016b21abcbbbeb
ar20190114 <skdr0724@gmail.com>
2022-12-10 19:54:08 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 
-createApp(App).use(store).use(router).mount("#app");
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(store).use(router).use(vuetify).mount("#app");
